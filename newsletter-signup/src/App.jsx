b8400bae9bc5720d54c93986e5358d3a184c0928
Jay Designs <izfaculty12@gmail.com>
2024-11-19 16:13:19 +0000

    Changes made

--- newsletter-signup/src/App.jsx
+++ newsletter-signup/src/App.jsx
@@ -5,11 +5,18 @@ import Lists from "./Lists";
 import Email from "./Email";
 
 function App() {
+  const [email, setEmail] = useState("");
+
+  function handleEmail(event) {
+    const value = event.target.vlaue;
+    setEmail(value);
+  }
+
   return (
     <>
       <div className="top-container w-full bg-"></div>
       <div className="p-[30px] flex flex-col gap-5">
-        <h1 className="text-[30px] font-bold text-Dark-slateGray">
+        <h1 className="text-[24px] font-bold text-Dark-slateGray">
           Stay Updated!
         </h1>
         <p className="text-[16px] text-Charcoal-Grey font-normal">
@@ -18,7 +25,7 @@ function App() {
         <Lists list={"Product discovery and  building what matters"} />
         <Lists list={"Measuring to ensure updates are a success"} />
         <Lists list={"And much more!"} />
-        <Email />
+        <Email handleEmail={handleEmail} email={email} />
       </div>
     </>
   );
--- newsletter-signup/src/Email.jsx
+++ newsletter-signup/src/Email.jsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Email() {
+function Email({ handleEmail, email }) {
+  const [emailError, setEmailError] = useState("");
+
+  function validateEmail(e) {
+    const value = e.target.value;
+    handleEmail(e);
+    if (!value) {
+      setEmailError("This field cannot be empty");
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+      setEmailError("valid email required");
+    } else {
+      setEmailError("");
+    }
+  }
   return (
-    <div className="flex flex-col relative mt-[30px] gap-1">
-      <label
-        className="text-[13px] font-semibold text-Dark-slateGray"
-        htmlFor=""
-      >
-        Email address
-      </label>
-      <input
-        className="p-[10px] pl-[20px] rounded-[10px] border-solid border-Grey border-[1px] outline-none"
-        type="email"
-        placeholder="email@company.com"
-      />
-      <input
-        className="font-semibold relative mt-[10px] cursor-pointer bg-Dark-slateGray text-White p-[15px] rounded-[10px]"
-        type="submit"
-        value={"Subscribe to monthly newsletter"}
-      />
-    </div>
+    <form action="">
+      <div className="flex flex-col relative mt-[30px] gap-1">
+        <div className="flex justify-between">
+          <label
+            className="text-[13px] font-semibold text-Dark-slateGray"
+            htmlFor=""
+          >
+            Email address
+          </label>
+          <span className="error-message text-[13px] text-primary-tomato">
+            {emailError}
+          </span>
+        </div>
+
+        <input
+          className={`error-message ${
+            emailError ? "border-primary-tomato" : "border-Grey"
+          } p-[10px] pl-[20px] rounded-[10px] border-solid border-Grey border-[1px] outline-none`}
+          type="email"
+          placeholder="email@company.com"
+          onChange={validateEmail}
+          value={email}
+        />
+        <input
+          className="font-semibold relative mt-[10px] cursor-pointer bg-Dark-slateGray text-White p-[15px] rounded-[10px]"
+          type="submit"
+          value={"Subscribe to monthly newsletter"}
+        />
+      </div>
+    </form>
   );
 }
 
